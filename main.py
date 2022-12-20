51d114ae5733175558281498f0fcd8040eb51c79
Caleb <csimmons718@bears.shawu.edu>
2022-12-20 00:11:43 -0800

    Create car code section -> new function to create_car & apply features, to_string class function to display car features

--- main.py
+++ main.py
@@ -10,6 +10,30 @@ class Car:
         self.color = color
         self.year = year
 
+    def to_string(self) -> str:
+        output = ''
+        output += 'Make: ' + self.make + '\n'
+        output += 'Model: ' + self.model + '\n'
+        output += '#ofDoors: ' + self.num_doors + '\n'
+        output += 'Color: ' + self.color + '\n'
+        output += 'Year: ' + self.year + '\n'
+
+
+# Function to create car object
+def create_car() -> Car:
+    print('Please fill out the form below to create your new car.')
+    make = input('Make: ')
+    model = input('Model: ')
+    num_doors = input('Number of Doors: ')
+    color = input('Color: ')
+    year = input('Year: ')
+
+    new_car = Car(make, model, num_doors, color, year)
+    return new_car
+
+
+# Initialize empty array to store car objects
+car_array: Car = []
 
 # Main Menu
 while True:
@@ -24,7 +48,11 @@ while True:
 
     if choice == '1':  # Create car
         print('Lets create a car!')
+        # Create new car calling function -> add to array
+        car = create_car()
+        car_array.append(car)
 
+        print('*' + car.make + ' ' + car.model + ' successfully created & added to list*')
     elif choice == '2':  # Delete car
         # Delete Function
         print('Delete Car')
